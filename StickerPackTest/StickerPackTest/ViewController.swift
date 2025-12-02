72995ef3419b1b944f287d81eed1a385755b3f8a
Eugene Kolesnikov <eugene-kolesnikov@hotmail.com>
2025-12-02 12:17:25 +0300

    init

---
+++ StickerPackTest/StickerPackTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StickerPackTest
+//
+//  Created by e.a.kolesnikov on 02.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
